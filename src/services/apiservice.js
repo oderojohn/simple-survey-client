29ce3a8c4ed39a4b2c5b9141f33509954dac3079
John Odero <john.odero26@gmail.com>
2025-04-12 22:15:43 +0300

    updated API base url

--- src/services/apiservice.js
+++ src/services/apiservice.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://127.0.0.1:8000/api";
+const API_BASE = "https://simple-survey-client.netlify.app/api";
 
 export const fetchQuestions = async () => {
   try {
