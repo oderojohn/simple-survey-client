a18964c8b2e74e613f11c157a0f27afa848d8c7f
John Odero <john.odero26@gmail.com>
2025-04-12 22:20:30 +0300

    updated API base url

--- src/services/apiservice.js
+++ src/services/apiservice.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "https://simple-survey-client.netlify.app/api";
+const API_BASE = "https://sky-survey-backend.onrender.com/api";
 
 export const fetchQuestions = async () => {
   try {
